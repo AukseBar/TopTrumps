361887ba73d41b1493497f9106e48851828ad3ef
aukse <aukse@LAPTOP-F23AIU21>
2017-01-18 20:41:00 +0000

    Some methods. Will prob change later.

--- Card.java
+++ Card.java
@@ -1,4 +1,35 @@
-
+/***/
 public class Card {
 
+	private String title;
+	private int attrib1, attrib2, attrib3, attrib4, attrib5; // specific names instead?
+	
+	/**constructor
+	 * @param String containing card info
+	 * */
+	public Card (String infor) {	
+		//takes a string and breaks it into chunks
+
+    	String [] info=infor.split("\\s+");
+    	this.title=info[0];  // title is the first word in the String
+    	this.attrib1=Integer.parseInt(info[1]);  // 
+    	this.attrib2=Integer.parseInt(info[2]);
+    	this.attrib2=Integer.parseInt(info[3]);
+    	this.attrib2=Integer.parseInt(info[4]);
+    	this.attrib2=Integer.parseInt(info[5]);
+	}
+	
+
+	public String displayCard(){
+		String formattedString=(String.format());
+		
+		
+		return formattedString;
+	}
+	
+	
+	//getter methods
+	
+	
+	
 }
--- Deck.java
+++ Deck.java
@@ -1,4 +1,32 @@
 
 public class Deck {
 
+	private Card[] mainDeck;
+	private int deckTopPointer=0;
+	
+	public Deck(){
+		this.mainDeck= new Card[40];
+	}
+	
+	public void addCard(String cardInfo){
+		Card newCard= new Card(cardInfo);
+		this.mainDeck[deckTopPointer]=newCard;
+		this.deckTopPointer++;
+	}
+	
+	public void removeCard(){
+		deckTopPointer--;
+		mainDeck[deckTopPointer]=null;
+	}
+	
+	public void shuffleDeck(){
+		
+	}
+	
+	public Card getTopCard(){
+		
+		return mainDeck[deckTopPointer-1];
+	}
+	
+	
 }
