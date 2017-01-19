b1c04f0d4c1b4858ef26985ebf10fec34c5660b9
Calum Haszeldine <calum.thain@gmail.com>
2017-01-19 20:56:29 +0000

    Worked on Game; filled out nextRound and calculateRound stubs, begun results methods

--- Game.java
+++ Game.java
@@ -1,3 +1,7 @@
+import java.util.Random;
+
+import javax.print.attribute.standard.NumberOfDocuments;
+
 /**
  * Defines the central game model for the Top Trumps card game implementation
  * Initialises the Players and Decks then runs the main game logic responding to
@@ -8,30 +12,36 @@
 
 public class Game {
 
-	protected static final int HUMAN_PLAYER = 0;
+	protected static final int HUMAN_PLAYER = 0;	// Human player is always first position in player array
 	private boolean gameInProgress = false;
 	private Deck communalPile = new Deck();
-	private Deck initialDeck;
+	private Deck mainDeck;
 	private Player[] player;
+	private Player currentPlayer;
+	private int numOfCompPlayers;
 	private int totalRounds;
 	private int draws;
-	private int currentPlayer;
 
 	/**
 	 * Constructor for a Game instance
-	 * @param initialDeck a Deck instance containing the full deck of cards as read in
+	 * @param mainDeck a Deck instance containing the full deck of cards as read in
 	 * from a seperate file */
-	public Game(Deck initialDeck) {
-		this.initialDeck = initialDeck;
+	public Game(Deck mainDeck) {
+		this.mainDeck = mainDeck;
 	}
 
+	/**
+	 * Initialises a new game state and begins the first round of a new game
+	 * @param numOfCompPlayers the number of opponents chosen by the human player when they start the game */
 	private void startGame(int numOfCompPlayers) {
+		this.numOfCompPlayers = numOfCompPlayers;
 		gameInProgress = true;
 		totalRounds = 0;
 		draws = 0;
-		player = new player[numOfCompPlayers + 1];
+		player = new Player[numOfCompPlayers + 1];
+		player[HUMAN_PLAYER].setHuman(true);		//**** Relies on an external unimplemented or changeable design
 		initPlayerDecks();
-		currentPlayer = randomiseFirstPlayer();
+		currentPlayer = player[randomiseFirstPlayer()];
 		nextRound();
 	}
 
@@ -42,10 +52,12 @@ public class Game {
 		return r.nextInt(numOfCompPlayers + 1);
 	}
 
+	/**
+	 * Iterates through each player dealing them the next top card from the deck until there are no cards left */
 	private void initPlayerDecks() {
-		initialDeck.shuffleDeck();
-		for(int i = 0, int j = 0; i < initialDeck.length; i++) {
-			player[j].getDeck().addCard(initialDeck.getTopCard());
+		mainDeck.shuffleDeck();
+		for(int i = 0, j = 0; i < mainDeck.getSize(); i++) {				//**** Relies on an external unimplemented or changeable design 
+			player[j].getDeck().addCard(mainDeck.getNextCard());			//**** Relies on an external unimplemented or changeable design 
 			if(j <= numOfCompPlayers) {
 				j++;
 			}
@@ -55,25 +67,65 @@ public class Game {
 		}
 	}
 
+	/**
+	 * Begins the next round; if currentPlayer is human it will wait for input, otherwise it will call for the computer 
+	 * player to choose a category then initiate the comparison between players  */
 	private void nextRound() {
-		if(currentPlayer != HUMAN_PLAYER) {
-			player[currentPlayer].chooseCategory();
+		totalRounds++;
+		if(!currentPlayer.isHuman()) {			//**** Relies on an external unimplemented or changeable design
+			calculateRoundResult(currentPlayer.chooseCategory());		// If currentPlayer is computer		//**** Relies on an external unimplemented or changeable design
 		}
 		else {
-			// Do nothing, wait for human player to make choice and activate compareCards()
+			// Do nothing, wait for human player to confirm choice and thereby activate calculateRoundResult() directly
 		}
 	}
 
-	private void compareCards() {
+	/**
+	 * NOT FINISHED */
+	private void calculateRoundResult(int chosenCategory) {
 
-	}
+		int roundWinner = 0;		// Be optimistic, assume human player will win most of time
+		int highestValue = player[0].getDeck.getTopCard().getCategoryValue(chosenCategory);		//**** Relies on an external unimplemented or changeable design
+		int comparedPlayerValue;
+		int drawValue = 0;
+		
+		for(int i = 1; i <= numOfCompPlayers; i++) {
+			comparedPlayerValue = player[i].getDeck.getTopCard().getCategoryValue(chosenCategory);
 
-	private void roundWon() {
-
+			if(comparedPlayerValue > highestValue) {
+				highestValue = comparedPlayerValue;
+				roundWinner = i;
+			}
+			else if(comparedPlayerValue == highestValue) {
+				drawValue = highestValue;
+			}
+			// Else compared value is < highest value so continue loop
+		}
+		
+		if(highestValue == drawValue) {
+			roundDraw();
+		}
+		else {
+			roundWon(player[roundWinner]);
+		}
 	}
 
 	private void roundDraw() {
 
+	}
+	
+	/**
+	 * NOT FINISHED
+	 * @param winner
+	 */
+	private void roundWon(Player winner) {
+		if(winner != currentPlayer) {
+			currentPlayer = winner;
+			// Give new currentPlayer everyone's top card (including their own as it goes to bottom)
+		}
+		else {
+			// Check if game has been won
+		}
 	}
 
 	private void endGame() {
@@ -84,7 +136,7 @@ public class Game {
 		return gameInProgress;
 	}
 
-	public int getCurrentPlayer() {
+	public Player getCurrentPlayer() {
 		return currentPlayer;
 	}
 }
