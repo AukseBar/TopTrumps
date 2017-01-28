1d068901da90ffc12cc651ee888fa85ed01bc773
Katie Glynn <katie.glynn@hotmail.co.uk>
2017-01-28 04:08:04 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,77 +1,118 @@
 import java.awt.event.ActionEvent;
+import java.util.Arrays;
 
 import javax.swing.JFrame;
 
-/* Indicates if player is human or not (uses boolean. how?), selects category for players (human or computer).
+/* Indicates if player is human or not, selects category for players (human or computer).
  * Returns int for roundsWon by each player. 
  * 
  * 
  * Maybe create additional class to manage array of players? As with Card/Deck classes.
  */
 
+/*If player array position == 0, then player is human. Human is always first player in array layout 
+/* of the class Game.
+ * 
+ *  isHuman == true if human is first instance in array.
+ *  all players afterwards are computer players.*/
 
-public class Player extends JFrame {   /* extend JFrame due to button pressed for roundsWon?
- 											& selection of category in GUI. */
-	
-	boolean human = false;
-	private int roundsWon;
-	private int hCategory;
-	private int CompCategory;
-	private Deck playerHand = new Deck();
 
-	
-	
-	
-	private void chooseCategory() {   
-		
-		if (human = false) { /* Automatically elects category on card with highest value?  */
-			Deck cards = new Deck();
-			int compCategory  = Math.max(cards.getAttrib1, cards.getAttrib2, cards.getAttrib3, cards.getAttrib4, cards.getAttrib5);
-			/* Math.max selects highest value. */
-		}
-		
-		else if (human = true) { 
-			
-		
-			public void actionPerformed(ActionEvent event) {
-				
-				if (event.getSource() == btnPlayCard)
-			
-			int hCategory = getJscrollpane; /* Needs category box in GUI to select from for HPlayer. */
-			
-		}}
-		
-	}
-	
 
-	public boolean setHuman(boolean b) {
+public class Player {
+	
+	// Hplayer is always first position (0) in player array.
+		protected static final int HUMAN_PLAYER = 0;
+		
+		private int numOfCompPlayers;
+		private String hCategory;
+		private String cCategory;
+		boolean human = false;
+		private int roundsWon;
+		private Deck playerHand = new Deck();
+		private Player[] players;
+		
+		
+	public boolean isHuman(int numOfCompPlayers){
+		
+	int[] players = {numOfCompPlayers + 1};
+	
+		if (players[HUMAN_PLAYER]{
+		
 		boolean human = true;
 		return human;
+		}
+	}
+
+	
+	
+	public void chooseHcategory(){ // Separate category for human player.
+		
+		GUI frame = new GUI();
+			
+			if (GUI.radioButton_0.getAction() != null){  //Selects radio button value if not null.
+				String hCategory = GUI.radioButton_0.getText();	
+			}
+			else if (GUI.getRadioButton_1().getAction() != null){
+				String hCategory = GUI.radioButton_1.getText();
+			}
+			else if (GUI.getRadioButton_2().getAction() != null){
+				String hCategory = GUI.getRadioButton_2().getText();
+			}
+			else if (GUI.getRadioButton_3().getAction() != null){
+				String hCategory = GUI.getRadioButton_3().getText();
+			}
+			else if (GUI.getRadioButton_4().getAction() != null){
+				String hCategory = GUI.getRadioButton_4().getText();
+			}
+	}
+
+	
+	public int chooseCcategory(){  //selects category for computer player
+		
+		Card cards = new Card(cCategory);
+		
+		//Card returnThisOne; Select top card instead? 
+		
+		int compCategory  = Math.max(cards.getCategoryValue(Card.attrib1), cards.getCategoryValue(Card.attrib2), cards.getCategoryValue(Card.attrib3), cards.getCategoryValue(Card.attrib4), cards.getCategoryValue(Card.attrib5));
+		// Math.max selects highest value. 
+		
+		return compCategory;
+	}
+
+	public void setHuman(boolean b) {
+		// TODO Auto-generated method stub
+		// game class refers to 'setHuman'
+	}
+	
+	public void getDeck(int numOfCompPlayers){   // Makes Player & Deck completely dependent.
+					
+		for (players[numOfCompPlayers + 1]){
+			
+			Deck hand = new Deck();  
+			// then split deck.
+		}
 		
 	}
 	
-	public getDeck(){  /* Method gets deck for each individual player. Makes Player & Deck completely dependent.
-					*/
-	
+	private void transferCard(){   // Method transfers cards between players/communal deck upon rounds being completed. 
 		
+		// get top card from non-round winners, transfer to round winner.
+		
+			
 	}
 	
-	private void transferCard(){   /* Method transfers cards between players/communal deck upon rounds being completed. */
-		
-	}
-	
-	
-	/* Returns int of roundsWon */
+
 	
 	public int getRoundsWon() {
 		
 		
-		if (player = getRoundWinner){ /* Relies on external class/method */
+		if (players[HUMAN_PLAYER] = Game.getRoundWinner){  //Relies on external class/method 
 		
-		this.roundsWon++; /* increments roundsWon */
+		this.roundsWon++;  //increments roundsWon 
 		}
 		return roundsWon;
 	}
-	
-
 }
+
+/*}
+*/
