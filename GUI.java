eae5c2d173faef4a96d319613d50a8ba783f379f
T <tespo@btinternet.com>
2017-01-29 15:46:53 +0000

    Tidied up radiobuttons

--- GUI.java
+++ GUI.java
@@ -1,56 +1,27 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
-import javax.swing.JLabel;
-import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
-import javax.swing.ButtonModel;
-
-import java.awt.GridLayout;
-import javax.swing.SwingConstants;
-
-import java.awt.Font;
-import java.awt.Color;
-import java.awt.Toolkit;
+import java.awt.*;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-// for deck:
+//for deck:
 import java.io.FileReader;
 import java.util.Scanner;
-
-import javax.swing.JRadioButton;
-import java.awt.FlowLayout;
-// @ TODO use * to shorten imports
 
 public class GUI extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
 	private JButton btnNewGame,btnPlayCard,btnViewStats,btnSaveStats;
-	private JTextArea areaCurrentStats,areaGameMessages,areaAttributes;
+	private JTextArea areaCurrentStats,areaGameMessages;
 	private JComboBox comboBoxPlayers;
-	private JLabel lblNumberOfCpu,lblCurrentPlayers,lblName,lblHeight,lblWeight,lblLength,lblFerocity,lblIntelligence,lblBlank;
-	private JPanel panel_choice;
-	private JRadioButton radioButton_0;
-	private JRadioButton radioButton_1;
-	private JRadioButton radioButton_2;
-	private JRadioButton radioButton_3;
-	private JRadioButton radioButton_4;
-	//private JLabel label;
-	private ButtonGroup group;
-
+	private JLabel lblNumberOfCpu,lblCurrentPlayers,lblBlank;
+	private JRadioButton radioButton_1, radioButton_2, radioButton_3, radioButton_4, radioButton_5;
+	private JTextField tfAttrib1,tfAttrib2,tfAttrib3,tfAttrib4,tfAttrib5,tfAttrib6;
+	private ButtonGroup radiogroup;
+	
 	//for deck
 	private Deck gameDeck;
 	private final int deckSize=40;
-
-	// @ TODO Add in all field declarations ie private...thingy and this.thingy
+	
 	/**
 	 * Create the frame.
 	 */
@@ -63,7 +34,6 @@ public class GUI extends JFrame implements ActionListener {
 		// Main Content Pane
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(102, 153, 153));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
@@ -78,31 +48,30 @@ public class GUI extends JFrame implements ActionListener {
 	public void layoutTop(){
 		// Top 'menu' panel
 		JPanel panel_top = new JPanel();
-		panel_top.setBackground(new Color(102, 153, 153));
+		//Layout and colour
+		panel_top.setBackground(new Color(77, 132, 96));
 		contentPane.add(panel_top, BorderLayout.NORTH);
-		
+		// Buttons and their names
 		this.btnNewGame = new JButton("New Game");
-		btnNewGame.setBackground(new Color(153, 204, 204));
-		panel_top.add(btnNewGame);
-		
-		this.lblNumberOfCpu = new JLabel("CPU Players:");
-		panel_top.add(lblNumberOfCpu);
-		
+		this.lblNumberOfCpu = new JLabel("Number of CPU Players:");
 		String [] NoOfPlayers = { "1", "2", "3", "4"};
-		
 		this.comboBoxPlayers = new JComboBox(NoOfPlayers);
-
-		//TEST DATA FOR CPU PLAYER NUMBERS NEEDS ALTERED////
-	
-		///////////////////////////////
-		panel_top.add(comboBoxPlayers);
-		
 		this.btnViewStats = new JButton("View Stats");
-		btnViewStats.setBackground(new Color(153, 204, 204));
+		this.btnSaveStats = new JButton("Save Stats");
+		// Button Colour settings
+		this.btnNewGame.setBackground(new Color(134, 199, 156));
+		this.btnViewStats.setBackground(new Color(134, 199, 156));
+		this.btnSaveStats.setBackground(new Color(134, 199, 156));
+		// Add all onto layout (in order)
+		panel_top.add(btnNewGame);
+		panel_top.add(lblNumberOfCpu);
+		panel_top.add(comboBoxPlayers);
 		panel_top.add(btnViewStats);
-		
+		panel_top.add(btnSaveStats);
+		// Listeners for button presses
 		this.btnNewGame.addActionListener(this);
 		this.btnViewStats.addActionListener(this);
+		this.btnSaveStats.addActionListener(this);
 		}
 	
 	public void layoutMiddle(){
@@ -112,159 +81,145 @@ public class GUI extends JFrame implements ActionListener {
 		panel_middle.setLayout(new GridLayout());
 		
 		// Left side sub-panel for current game statistics
+		// Panel properties
 		JPanel subpanel_current_stats = new JPanel();
-		subpanel_current_stats.setBackground(new Color(102, 153, 153));
+		subpanel_current_stats.setBackground(new Color(134, 199, 156));
 		panel_middle.add(subpanel_current_stats);
-		subpanel_current_stats.setLayout(new BoxLayout(subpanel_current_stats, BoxLayout.Y_AXIS));
-		
-		this.lblCurrentPlayers = new JLabel("      Current Game Statistics:");
-		lblCurrentPlayers.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCurrentPlayers.setBackground(new Color(102, 153, 153));
-		lblCurrentPlayers.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		subpanel_current_stats.add(lblCurrentPlayers);
-		
+		subpanel_current_stats.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		// Label
+		this.lblCurrentPlayers = new JLabel("Current Game Statistics:");
+		//Current Statistics box properties
 		this.areaCurrentStats = new JTextArea();
-		areaCurrentStats.setBackground(new Color(153, 204, 204));
-		areaCurrentStats.setEditable(false); // Toggle to allow editing
-		areaCurrentStats.setRows(8);
-		areaCurrentStats.setColumns(8);
+		
+		this.areaCurrentStats.setBackground(new Color(134, 199, 156));
+		this.areaCurrentStats.setEditable(false); // Toggle to allow editing
+		this.areaCurrentStats.setRows(8);
+		this.areaCurrentStats.setColumns(8);
+		// Silliness HERE
+		this.areaCurrentStats.setFont(new Font("Monospaced", Font.BOLD, 13));
+		String dino1 = "            __\n           / _)\n    .-^^^-/ /\n __/       /\n<__.|_|-|_|";
+		String dino2 = "             /\\\r\n           /\\  /\\\r\n       /\\/\\      /\\\r\n |||..^            ^^o\r\n ------__\\ /---\\ /--~\r\n          | |    ||\r\n          --*    -*";
+		this.areaCurrentStats.setText(dino1 + "\n\n" + dino2);
+		
+		//Add all to panel (ordered)
+		subpanel_current_stats.add(lblCurrentPlayers);
 		subpanel_current_stats.add(areaCurrentStats);
 		
-		// Middle sub-panel for the fixed categories
-		JPanel subpanel_categories = new JPanel();
-		subpanel_categories.setBackground(new Color(102, 153, 153));
-		panel_middle.add(subpanel_categories);
-		GridLayout gl_subpanel_categories = new GridLayout();
-		gl_subpanel_categories.setColumns(1);
-		gl_subpanel_categories.setRows(0);
-		subpanel_categories.setLayout(gl_subpanel_categories);
-		
-		this.lblName = new JLabel("Name:");
-		lblName.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblName.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		subpanel_categories.add(lblName);
-		
-		this.lblHeight = new JLabel("Height:");
-		lblHeight.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblHeight.setHorizontalAlignment(SwingConstants.TRAILING);
-		subpanel_categories.add(lblHeight);
-		
-		this.lblWeight = new JLabel("Weight:");
-		lblWeight.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblWeight.setHorizontalAlignment(SwingConstants.TRAILING);
-		subpanel_categories.add(lblWeight);
-		
-		this.lblLength = new JLabel("Length:");
-		lblLength.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblLength.setHorizontalAlignment(SwingConstants.TRAILING);
-		subpanel_categories.add(lblLength);
-		
-		this.lblFerocity = new JLabel("Ferocity:");
-		lblFerocity.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblFerocity.setHorizontalAlignment(SwingConstants.TRAILING);
-		subpanel_categories.add(lblFerocity);
-		
-		this.lblIntelligence = new JLabel("Intelligence:");
-		lblIntelligence.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblIntelligence.setHorizontalAlignment(SwingConstants.TRAILING);
-		subpanel_categories.add(lblIntelligence);
-		
-		// Right sub-panel for showing the attributes read from a card
+	
+		//Sub-panel for showing the attributes read from a card
 		JPanel subpanel_show_attributes = new JPanel();
-		subpanel_show_attributes.setBackground(new Color(153, 204, 204));
+		subpanel_show_attributes.setBackground(new Color(134, 199, 156));
+		subpanel_show_attributes.setLayout(new GridLayout(0,1,0,0));
 		panel_middle.add(subpanel_show_attributes);
-		subpanel_show_attributes.setLayout(new BoxLayout(subpanel_show_attributes, BoxLayout.X_AXIS));
 		
-		this.areaAttributes = new JTextArea();
-		areaAttributes.setBackground(new Color(153, 204, 204));
-		areaAttributes.setEditable(false); // Toggle to allow editing
-		areaAttributes.setRows(17);
-		areaAttributes.setColumns(15);
-		subpanel_show_attributes.add(areaAttributes);
+		this.lblBlank = new JLabel("Name:    ");
+		lblBlank.setHorizontalAlignment(SwingConstants.RIGHT);
 		
 		
-		// PoPULATE CARD AREA WITH TEST STRING
-		String testing = "\r\n TRex \r\n\r\n\r\n\r\n 6 \r\n\r\n\r\n 6 \r\n\r\n\r\n 12 \r\n\r\n\r\n\r\n 9 \r\n\r\n\r\n\r\n 9";
-		areaAttributes.setText(testing);
-		////////////////////////////////////////////
+		this.radioButton_1 = new JRadioButton("Height");
+		this.radioButton_2 = new JRadioButton("Weight");
+		this.radioButton_3 = new JRadioButton("Length");
+		this.radioButton_4 = new JRadioButton("Ferocity");
+		this.radioButton_5 = new JRadioButton("Intelligence");
 		
-		// NEEDS REFORMATED WITH THISES AND PRIVATES
-		panel_choice = new JPanel();
-		panel_choice.setBackground(new Color(153, 204, 204));
-		panel_choice.setForeground(new Color(153, 204, 204));
-		subpanel_show_attributes.add(panel_choice);
-		panel_choice.setLayout(new GridLayout(0, 1, 0, 0));
+		radioButton_1.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		radioButton_2.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		radioButton_3.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		radioButton_4.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		radioButton_5.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
 		
-		this.lblBlank = new JLabel("");
-		lblBlank.setForeground(new Color(153, 204, 204));
-		panel_choice.add(lblBlank);
+		radioButton_1.setBackground(new Color(134, 199, 156));
+		radioButton_2.setBackground(new Color(134, 199, 156));
+		radioButton_3.setBackground(new Color(134, 199, 156));
+		radioButton_4.setBackground(new Color(134, 199, 156));
+		radioButton_5.setBackground(new Color(134, 199, 156));
 		
-		this.radioButton_0 = new JRadioButton("");
-		radioButton_0.setBackground(new Color(153, 204, 204));
-		panel_choice.add(radioButton_0);
+		// Add all in order
+		subpanel_show_attributes.add(lblBlank);
+		subpanel_show_attributes.add(radioButton_1);
+		subpanel_show_attributes.add(radioButton_2);
+		subpanel_show_attributes.add(radioButton_3);
+		subpanel_show_attributes.add(radioButton_4);
+		subpanel_show_attributes.add(radioButton_5);
 		
-		this.radioButton_1 = new JRadioButton("");
-		radioButton_1.setBackground(new Color(153, 204, 204));
-		panel_choice.add(radioButton_1);
-		
-		this.radioButton_2 = new JRadioButton("");
-		radioButton_2.setBackground(new Color(153, 204, 204));
-		panel_choice.add(radioButton_2);
-		
-		this.radioButton_3 = new JRadioButton("");
-		radioButton_3.setBackground(new Color(153, 204, 204));
-		panel_choice.add(radioButton_3);
-		
-		this.radioButton_4 = new JRadioButton("");
-		radioButton_4.setBackground(new Color(153, 204, 204));
-		panel_choice.add(radioButton_4);
-		
-		this.group = new ButtonGroup();
-		group.add(radioButton_0);
-		group.add(radioButton_1);
-		group.add(radioButton_2);
-		group.add(radioButton_3);
-		group.add(radioButton_4);
+		this.radiogroup = new ButtonGroup();
+		radiogroup.add(radioButton_1);
+		radiogroup.add(radioButton_2);
+		radiogroup.add(radioButton_3);
+		radiogroup.add(radioButton_4);
+		radiogroup.add(radioButton_5);
 		
 		// TESTING BUTTON ACTIONS
-		this.radioButton_0.setActionCommand("Height");
-		this.radioButton_1.setActionCommand("Weight");
-		this.radioButton_2.setActionCommand("Length");
-		this.radioButton_3.setActionCommand("Ferocity");
-		this.radioButton_4.setActionCommand("Intelligence");
+		this.radioButton_1.setActionCommand("Height");
+		this.radioButton_2.setActionCommand("Weight");
+		this.radioButton_3.setActionCommand("Length");
+		this.radioButton_4.setActionCommand("Ferocity");
+		this.radioButton_5.setActionCommand("Intelligence");
 		/////////////////////////////////////////////////
+		
+		
+		//Sub-panel for the actual data
+		JPanel subpanel_categories = new JPanel();
+		subpanel_categories.setBackground(new Color(134, 199, 156));
+		subpanel_categories.setLayout(new GridLayout(0,1,0,0));
+		panel_middle.add(subpanel_categories);
+		
+		this.tfAttrib1 = new JTextField();
+		this.tfAttrib2 = new JTextField();
+		this.tfAttrib3 = new JTextField();
+		this.tfAttrib4 = new JTextField();
+		this.tfAttrib5 = new JTextField();
+		this.tfAttrib6 = new JTextField();
+		// Makes it blend into background colour
+		tfAttrib1.setBorder(null);
+		tfAttrib2.setBorder(null);
+		tfAttrib3.setBorder(null);
+		tfAttrib4.setBorder(null);
+		tfAttrib5.setBorder(null);
+		tfAttrib6.setBorder(null);
+		// Set box colours
+		tfAttrib1.setBackground(new Color(134, 199, 156));
+		tfAttrib2.setBackground(new Color(134, 199, 156));
+		tfAttrib3.setBackground(new Color(134, 199, 156));
+		tfAttrib4.setBackground(new Color(134, 199, 156));
+		tfAttrib5.setBackground(new Color(134, 199, 156));
+		tfAttrib6.setBackground(new Color(134, 199, 156));
+		// Add all in order
+		subpanel_categories.add(tfAttrib1);
+		subpanel_categories.add(tfAttrib2);
+		subpanel_categories.add(tfAttrib3);
+		subpanel_categories.add(tfAttrib4);
+		subpanel_categories.add(tfAttrib5);
+		subpanel_categories.add(tfAttrib6);
+		
+		
 		}
 	
 	public void layoutBottom(){
 		
 		// Bottom panel for in-game messages and save game button
 		JPanel panel_bottom = new JPanel();
-		panel_bottom.setBackground(new Color(102, 153, 153));
+		panel_bottom.setBackground(new Color(77, 132, 96));
 		contentPane.add(panel_bottom, BorderLayout.SOUTH);
-		panel_bottom.setLayout(new BoxLayout(panel_bottom, BoxLayout.X_AXIS));
-		
+		panel_bottom.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		// Add a scrollpane
 		JScrollPane scrollPane = new JScrollPane();
 		panel_bottom.add(scrollPane);
 		
 		this.areaGameMessages = new JTextArea();
-		areaGameMessages.setBackground(new Color(153, 204, 204));
+		areaGameMessages.setBackground(new Color(134, 199, 156));
 		areaGameMessages.setEditable(false); // Toggle to allow editing
 		areaGameMessages.setRows(5);
-		areaGameMessages.setColumns(30);
+		areaGameMessages.setColumns(25);
 		scrollPane.setViewportView(areaGameMessages);
 		
 		this.btnPlayCard = new JButton("Play Card!");
-		btnPlayCard.setBackground(new Color(153, 204, 204));
+		btnPlayCard.setPreferredSize(new Dimension(160, 50));
+		btnPlayCard.setBackground(new Color(134, 199, 156));
 		panel_bottom.add(btnPlayCard);
-		
-		this.btnSaveStats = new JButton("Save Stats");
-		btnSaveStats.setBackground(new Color(153, 204, 204));
-		panel_bottom.add(btnSaveStats);
-		
 		
 		// Action listeners on the bottom
 		this.btnPlayCard.addActionListener(this);
-		this.btnSaveStats.addActionListener(this);
 		
 	}
 	
@@ -300,7 +255,7 @@ public class GUI extends JFrame implements ActionListener {
 	    	
 	    	
 	    	// TESTING FOR SELECTIoN OF A BUTTON
-	    	ButtonModel buttonModel = this.group.getSelection();
+	    	ButtonModel buttonModel = this.radiogroup.getSelection();
 	    	String actionCommand = buttonModel.getActionCommand();
             System.out.println("Selected Button: " + actionCommand);
             ////////////////////////////////////////////////
