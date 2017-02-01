c93f50176fd949f219e71936d42b21444fe4fbdc
Katie Glynn <katie.glynn@hotmail.co.uk>
2017-02-01 14:38:11 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -6,15 +6,7 @@ import javax.swing.JFrame;
 /* Indicates if player is human or not, selects category for players (human or computer).
  * Returns int for roundsWon by each player. 
  * 
- * 
- * Maybe create additional class to manage array of players? As with Card/Deck classes.
  */
-
-/*If player array position == 0, then player is human. Human is always first player in array layout 
-/* of the class Game.
- * 
- *  isHuman == true if human is first instance in array.
- *  all players afterwards are computer players.*/
 
 
 public class Player {
@@ -28,21 +20,40 @@ public class Player {
 		playerHand = new Deck();
 	}
 
-	public int chooseCategory(){  //selects category for computer player
+	public int chooseCategory(int categoryIn){  //selects category for computer player
+		
+		Card c = new Card(null);
+		int [] hValue = {c.getCategoryValue(1), c.getCategoryValue(2), c.getCategoryValue(3), c.getCategoryValue(4), c.getCategoryValue(5)};
+		
+		int max = hValue[0];
+		
+		for (int i = 1; i < hValue.length; i++)
+		{
+		     if (hValue[i] > max)
+		     {
+		      max = hValue[i];
+		     }
+		}
+	//	int highValue = Math.max(c.getCategoryValue(1), Math.max( c.getCategoryValue(2), Math.max(c.getCategoryValue(3), c.getCategoryValue(4))));
 
-		int compCategory  = Math.max(cards.getCategoryValue(Card.attrib1), cards.getCategoryValue(Card.attrib2), cards.getCategoryValue(Card.attrib3), cards.getCategoryValue(Card.attrib4), cards.getCategoryValue(Card.attrib5));
-		// Math.max selects highest value. 
-
+		
+		
+		
+		//needs to get attribute number, not highest value
+		
 		return compCategory;		//****** This would return the highest value, what is actually wanted to be returned though is the attribute number (1-5) that this value belongs to
 	}
 
 	public void transferCardTo(Deck deck) {   // Method transfers cards from this deck to players' decks/communal deck upon rounds being completed. 
 
-		// get top card and transfer to the bottom of the deck passed as the argument
+		playerHand.getTopCard();
+		playerHand.addCardToBottom(null);
 	}
 
 	public void wonRound() {
-		//
+		// increments rounds won.
+		
+		roundsWon++;
 	}
 	
 	public int getRoundsWon() {
