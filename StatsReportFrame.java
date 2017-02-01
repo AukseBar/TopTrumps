ce30de024921b1568ec8633d585637e310d53264
hollymaxwellpringle <1105443p@student.gla.ac.uk>
2017-02-01 17:46:17 +0000

    Update StatsReportFrame.java
    updated to include working connection and one working query, button issue still needs sorted

--- StatsReportFrame.java
+++ StatsReportFrame.java
@@ -1,67 +1,120 @@
 import java.awt.event.ActionEvent;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import java.sql.*;
 import javax.swing.JFrame;
 
 /* Accesses SQL database and returns stat values - depending on Jbuttons pressed in GUI.
 saveStatsToDatabase()
 getStatsFromDatabase()
 viewStats()
-
- * 
+ *
  */
 
 public class StatsReportFrame extends JFrame{
+       
+	private String dbname = "m_16_1105443p";
+	private String username = "m_16_1105443p";
+	private String password = "1105443Pp";
+	private Connection connection =null;
+	public String gamesPlayed;
 	
-	
-	public void saveStatsToDatabase(){
-		
-		public void actionPerformed(ActionEvent event) {
-			
-			if (event.getSource() == btnSaveStats)
-			{
-			
-			{
-				
-				String dbname = "postgres";
-				String username = "postgres";
-				String password = "";			/*input appropriate password */
-				Connection connection =null;
-				
-				try
-					{
-					connection =
-					DriverManager.getConnection("jdbc:postgresql://localhost:5433/" +
-					dbname,username, password);
-					}
-				
-					catch (SQLException e1)
-					{
-					System.err.println("Connection Failed!");
-					e1.printStackTrace();
-					return;
-					}
-				
-				Statement stmt = null;
-			}}
-		
-	}
-	
-	public int getStatsFromDatabase(){
-		
-		int stats =  0;
-		return stats;   // TEST - CHANGE AS APPROPRIATE
-		
-	}
-	
-	
-	public int viewStats(){
-		
-		int stats = 0;
-		return stats; // TEST - CHANGE AS APPROPRIATE
-	}
+
+	public StatsReportFrame ()
+	{
+
+	this("m_16_1105443p", "m_16_1105443p", "1105443p");
 	}
 
+	public StatsReportFrame (String dbname, String username, String password)
+	{
+
+		this.dbname = dbname;
+		this.username = username;
+		this.password = password;
+	}
+	
+	//set up connection
+	
+	public void newConnection()
+	{
+
+		try
+		{
+		connection = DriverManager.getConnection ("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/" + dbname, username, password);
+		} 
+	
+		catch(SQLException e) 
+		{
+			System.err.println("Connection Failed!");
+			e.printStackTrace();
+			return;
+		}
+	
+		if (connection != null) 
+		{
+			System.out.println("Connection successful");
+		} 
+		else
+		{
+			System.err.println("Failed to make connection!");
+		}
+	
+	}
+	
+	//begin queries to get stats 
+
+			public void getStats ()
+			{
+						
+			Statement stmt1 = null;
+			String query1 = "SELECT game_number FROM TopTrumpsData;";
+	
+			try 
+			{
+				stmt1 = connection.createStatement();
+				ResultSet rs1 = stmt1.executeQuery(query1);
+	
+			while (rs1.next())
+			{
+
+				String gamesPlayed = rs1.getString("game_number"); //realise this should be an int, can parse later 
+				System.out.println(gamesPlayed);
+
+			}
+			}
+			catch (SQLException e)
+			{
+				e.printStackTrace();
+				System.out.print("SQL statement not found");
+			}
+	
+	}
+					public void actionPerformed (ActionEvent ae) {
+	
+						if (ae.getSource() == GUI.btnViewStats) //???????? Y U DO DIS
+							newConnection();
+							getStats();
+					}
+
+		public void closeConnection ()
+		{
+		try 
+		{
+			connection.close();
+			System.out.println("Connection closed");
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			System.out.println("Connection could not be closed - SQL exception");
+		}
+	
+	
+		}
+
+        
+        public int viewStats(){
+               
+                int stats = 0;
+  
+                return stats; // TEST - CHANGE AS APPROPRIATE
+        }
