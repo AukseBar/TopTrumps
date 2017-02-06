c63df666561fa411721d5f7f8eb3109420927871
Aukse Barak <auksebarak@gmail.com>
2017-02-06 13:26:11 +0000

    changed attributes to array- doesnt affect anything outside Card class

--- Card.java
+++ Card.java
@@ -3,8 +3,8 @@
 public class Card {
 
 	private String title, infoText;
-	private int attrib1, attrib2, attrib3, attrib4, attrib5;
-	
+	private final int NrOfAttributes=5;
+	private int[] attribute= new int[NrOfAttributes];	
 	/**constructor
 	 * @param String containing card info
 	 * */
@@ -12,19 +12,18 @@ public class Card {
 		this.infoText=infor;
     	String [] info=infoText.split("\\s+");
     	this.title=info[0];					    // title is the first word in the String
-    	this.attrib1=Integer.parseInt(info[1]); // the next four chunks have attribute values
-    	this.attrib2=Integer.parseInt(info[2]);
-    	this.attrib3=Integer.parseInt(info[3]);
-    	this.attrib4=Integer.parseInt(info[4]);
-    	this.attrib5=Integer.parseInt(info[5]);
-    	// for testing draws::::::::::::::: this.attrib5=1;
+    	for (int i=0; i<NrOfAttributes; i++){	// put category keywords into attribute array
+    		attribute[i]=Integer.parseInt(info[i+1]);
+    	}
+
+    	// for testing draws::::::::::::::: this.attribute[4]=1;
 	}
 	
 
 	/**
 	 * returns a String formatted for being displayed on the screen
-	 * includes the title and values of card attributes*/
-	public String displayCard(){	
+	 * includes the title and value of card attributes*/
+	public String toString(){	
 		return infoText;  // return String used in constructor
 	}
 	
@@ -34,16 +33,7 @@ public class Card {
 	 * @param int [1..5] marks which attribute value
 	 * is requested*/
 	public int getCategoryValue(int categoryIn){
-		if (categoryIn==1)
-			return attrib1;
-		if (categoryIn==2)
-			return attrib2;
-		if (categoryIn==3)
-			return attrib3;
-		if (categoryIn==4)
-			return attrib4;
-		else  				 // must be 5
-			return attrib5;
+		return attribute[categoryIn-1];
 	}
 	
 	
